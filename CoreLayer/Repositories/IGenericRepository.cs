aaed17f5ec458f6110bfec666e2b3775cbf78d7a
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2023-12-21 01:51:26 +0300

    Core ve Shared Katmanına eklemeler yapıldı
    Core katmanında Repository class’ı oluşturularak ‘Generic’ yapılar yazıldı. Uygulamada alınan herhangi bir hata ya da başarılı isteklerin client’lar  tarafından bilinmesi adına, SharedLayer katmanında Response ve ErrorDto sınıfı oluşturuldu.

---
+++ CoreLayer/Repositories/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLayer.Repository
+{
+    public interface IGenericRepository<TEntitiy> where TEntitiy : class
+    {
+        Task<TEntitiy> GetByIdAsync(int id);
+
+        Task<IEnumerable<TEntitiy>> GetAllAsync();
+
+        IQueryable<TEntitiy> Where(Expression<Func<TEntitiy, bool>> predicate);
+
+        Task AddAsync(TEntitiy entitiy);
+
+        void Remove(TEntitiy entitiy);
+
+    }
+}
---
+++ SharedLibrary/Dtos/Response.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Dtos
+{
+    public class Response<T> where T : class
+    {
+        public T Data { get; private set; }
+        public int StatusCode { get; private set; }
+
+        [JsonIgnore]
+        public bool IsSuccesful { get; private set; }
+        public ErrorDto Error { get; private set; }
+
+
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccesful = true };
+        }
+
+        public static Response<T> Success(int statusCode)
+        {
+            return new Response<T> { Data = default, StatusCode = statusCode, IsSuccesful = true };
+        }
+
+
+        public static Response<T> Fail(ErrorDto errorDto, int statusCode)
+        {
+            return new Response<T>
+            {
+                Error = errorDto,
+                StatusCode = statusCode,
+                IsSuccesful = false
+            };
+        }
+
+        public static Response<T> Fail(string errorMesage, int statusCode, bool isShow)
+        {
+            var errorDto = new ErrorDto(errorMesage, isShow);
+            return new Response<T> { Error = errorDto, StatusCode = statusCode, IsSuccesful = false };
+        }
+
+
+    }
+
+}
