ec62f2e183782996b5d2682982ea621ad80a99dd
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2023-12-22 16:33:18 +0300

    Repository katmanı tamamlandı
    AppDbContext sınıfı oluşturularak Identity’i referans alınması sağlanıldı. Configuration işlemleri için sınıf oluşturuldu ve AppDbContext’ de Asembly ile yazılan sınıflar tanımlandı. GenericRepository class’ı oluşturuldu. DbSet ve DbContext sınıfa eklendikten sonra consuractor ile birlikte ilgili kodlar yazıldı.

---
+++ RepositoryLayer/Configurations/UserAppConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using CoreLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Configurations
+{
+    public class UserAppConfiguration : IEntityTypeConfiguration<UserApp>
+    {
+        public void Configure(EntityTypeBuilder<UserApp> builder)
+        {
+            builder.Property(x => x.City).HasMaxLength(200);
+        }
+    }
+}
---
+++ RepositoryLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,61 @@
+﻿using CoreLayer.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Repositories
+{
+    public class GenericRepository<TEntitiy> : IGenericRepository<TEntitiy> where TEntitiy : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<TEntitiy> _dbset;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbset = context.Set<TEntitiy>();
+        }
+
+        public async Task AddAsync(TEntitiy entitiy)
+        {
+            _dbset.AddAsync(entitiy);
+        }
+
+        public async Task<IEnumerable<TEntitiy>> GetAllAsync()
+        {
+            return await _dbset.ToListAsync();
+        }
+
+        public async Task<TEntitiy> GetByIdAsync(int id)
+        {
+            var entites = await _dbset.FindAsync(id);
+
+            if(entites!=null)
+            {
+                _context.Entry(entites).State = EntityState.Detached;
+            }
+
+            return entites;
+        }
+
+        public void Remove(TEntitiy entitiy)
+        {
+            _dbset.Remove(entitiy);
+        }
+
+        public TEntitiy Update(TEntitiy entitiy)
+        {
+            _context.Entry(entitiy).State = EntityState.Modified;
+            return entitiy;
+        }
+
+        public IQueryable<TEntitiy> Where(Expression<Func<TEntitiy, bool>> predicate)
+        {
+            return _dbset.Where(predicate);
+        }
+    }
+}
